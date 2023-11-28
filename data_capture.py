d2f21e33f3bb674349e8e8f668236b4afc91e57c
alantoris <alan.toris@gmail.com>
2023-11-28 00:44:33 -0300

    Removing Fenwick tree, replaced by simple structure matching with specification for time complexity

--- data_capture.py
+++ data_capture.py
@@ -1,36 +1,3 @@
-class FenwickTree:
-    """
-    Class to represent a FenwickTree
-    """
-    def __init__(self, size: int) -> None:
-        """
-        Create a FenwickTree with a fixed size
-        :return: (None)
-        """
-        self.tree: list[int] = [0] * (size + 1)
-
-    def update(self, idx: int) -> None:
-        """
-        Updates the Fenwick tree with the given value
-        :param idx: index at which the update is to be made
-        :return: None
-        """
-        while idx < len(self.tree):
-            self.tree[idx] += 1
-            idx += idx & -idx
-
-    def query(self, idx: int) -> int:
-        """
-        Calculate the amount of the elements until the index
-        :param idx: index until which the sum is calculated
-        :return: (int)
-        """
-        result = 0
-        while idx > 0:
-            result += self.tree[idx]
-            idx -= idx & -idx
-        return result
-
 class DataCapture:
     """
     Class to represent a DataCapture
@@ -42,54 +9,81 @@ class DataCapture:
         Create a DataCapture with a internal FenwickTree with a fixed length
         :return: (None)
         """
-        self.num_counts = FenwickTree(self.LENGHT)
+        self.num_counts = [0] * self.LENGHT
         self.total_count = 0
 
     def add(self, num: int) -> None:
         """
-        Add a element to the internal tree, and increse the total count
-        :param num: num that will be added to the tree
+        Add a element to the internal structure, and increse the total count
+        :param num: num that will be added to the array
         :return: (None)
         """
-        if num > 0  and num <= self.LENGHT:
+        if 0 <= num < self.LENGHT:
             self.total_count += 1
-            self.num_counts.update(num)
+            self.num_counts[num] += 1
         else:
             raise ValueError(
-                f"The value added to the DataCapture has to be a small integer between 0 and {self.LENGHT}")
+                f"The value added to the DataCapture has to be a small integer between 0 and {self.LENGHT - 1}.")
 
     def build_stats(self) -> 'Stats':
         """
         Create a object Stats for calculating the variables related to a index
         :return: (Stats)
         """
-        return Stats(self)
+        previous = 0
+        stats = []
+        for cant in self.num_counts:
+            less = previous + cant
+            stats.append(
+                {
+                    "cant": cant,
+                    "less": less                 
+                }
+            )
+            previous = less
+
+        return Stats(self, stats)
 
 
 class Stats:
     """
     Class to represent a Stats of a DataCapture
     """
-    def __init__(self, data_capture: DataCapture) -> None:
+    def __init__(self, data_capture: DataCapture, stats: list[dict]) -> None:
         """
-        Create a Statis with a DataCapture related
+        Create a Statis with a DataCapture and stats related
         :return: (None)
         """
         self.data_capture = data_capture
+        self.stats = stats
 
     def less(self, value: int) -> int:
         """
         Return the amount of the elements lowers than the value
         :return: (int)
         """
-        return self.data_capture.num_counts.query(value-1)
+        if 0 <= value < self.data_capture.LENGHT:
+            if value == 0:
+                return 0
+            else:
+                return self.stats[value-1]["less"]
+        else:
+            raise ValueError(
+                f"The value to query the number of minor elements must be between 0 and {self.data_capture.LENGHT - 1}.")
 
     def greater(self, value: int) -> int:
         """
         Return the amount of the elements greater than the value
         :return: (int)
         """
-        return self.data_capture.total_count - self.less(value+1)
+        if 0 <= value < self.data_capture.LENGHT:
+            if value == self.data_capture.LENGHT - 1:
+                return 0
+            else:
+                return self.data_capture.total_count - self.less(value+1)
+        else:
+            raise ValueError(
+                f"The value to query the number of greater elements must be between 0 and {self.data_capture.LENGHT - 1}.")
 
     def between(self, low: int, high: int) -> int:
         """
@@ -97,4 +91,13 @@ class Stats:
         greater than the second one
         :return: (int)
         """
-        return self.less(high+1) - self.less(low - 1) if low > 0 else self.less(high+1)
+        if high <= low:
+            raise ValueError(
+                f"The second argument has to be greater than the first one.")
+        elif 0 <= low < self.data_capture.LENGHT or 0 <= high < self.data_capture.LENGHT:
+            high_amount = self.data_capture.total_count if high == self.data_capture.LENGHT - 1 else self.less(high+1)
+            low_amount = self.less(low)
+            return high_amount - low_amount
+        else:
+            raise ValueError(
+                f"The limit values to consult the number of elements in the middle must be between 0 and {self.data_capture.LENGHT - 1}.")
--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestDataCapture(unittest.TestCase):
         capture.add(4)
         capture.add(6)
         stats = capture.build_stats()
-        
+
         self.assertEqual(stats.less(4), 2)
         self.assertEqual(stats.between(3, 6), 4)
         self.assertEqual(stats.greater(4), 2)
@@ -23,26 +23,27 @@ class TestDataCapture(unittest.TestCase):
         capture.add(300)
         capture.add(600)
         capture.add(998)
+        capture.add(998)
         capture.add(999)
         stats = capture.build_stats()
         
-        self.assertEqual(stats.less(1000), 6)
-        self.assertEqual(stats.greater(998), 1)
-        self.assertEqual(stats.between(998, 1000), 2)
+        self.assertEqual(stats.less(999), 6)
+        self.assertEqual(stats.greater(997), 3)
+        self.assertEqual(stats.between(998, 999), 3)
 
     def test_greater_border(self):
         capture = DataCapture()
-        capture.add(1000)
+        capture.add(999)
         stats = capture.build_stats()
         self.assertEqual(stats.less(4), 0)
-        self.assertEqual(stats.greater(999), 1)
-        self.assertEqual(stats.greater(1000), 0)
+        self.assertEqual(stats.greater(998), 1)
+        self.assertEqual(stats.greater(999), 0)
 
     def test_lower_border(self):
         capture = DataCapture()
-        capture.add(1)
+        capture.add(0)
         stats = capture.build_stats()
-        self.assertEqual(stats.less(2), 1)
+        self.assertEqual(stats.less(1), 1)
         self.assertEqual(stats.greater(999), 0)
 
     def test_value_error(self):
@@ -50,7 +51,39 @@ class TestDataCapture(unittest.TestCase):
         with self.assertRaises(ValueError) as _:
             capture.add(1001)
         with self.assertRaises(ValueError) as _:
-            capture.add(0)
+            capture.add(-1)
+    
+    def test_value_less_error(self):
+        capture = DataCapture()
+        capture.add(10)
+        stats = capture.build_stats()
+        with self.assertRaises(ValueError) as _:
+            stats.less(-1)
+        with self.assertRaises(ValueError) as _:
+            stats.less(1001)
+
+    def test_value_greater_error(self):
+        capture = DataCapture()
+        capture.add(10)
+        stats = capture.build_stats()
+        with self.assertRaises(ValueError) as _:
+            stats.greater(-1)
+        with self.assertRaises(ValueError) as _:
+            stats.greater(1001)
+
+    def test_value_between_error(self):
+        capture = DataCapture()
+        capture.add(10)
+        capture.add(15)
+        capture.add(20)
+        stats = capture.build_stats()
+        self.assertEqual(stats.between(10, 20), 3)
+        with self.assertRaises(ValueError) as _:
+            stats.between(-1, 20)
+        with self.assertRaises(ValueError) as _:
+            stats.between(10, 1001)
+        with self.assertRaises(ValueError) as _:
+            stats.between(20, 10)
 
 
 if __name__ == '__main__':
